9d397e24907d25fcf36d1813f10e7fcca9618b36
Richard Shackleton <richard.shackleton@idhl.co.uk>
2022-08-07 14:17:07 +0100

    Update article list with extra data

--- app/routes/articles/index.tsx
+++ app/routes/articles/index.tsx
@@ -1,11 +1,15 @@
 import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
+import imageUrlBuilder from '@sanity/image-url';
 import type { ArticleModel, GetArticlesResult } from '~/queries/GetArticles';
 import { GetArticles } from '~/queries/GetArticles';
-import { graphqlClient } from '~/sanity/client';
+import { client, graphqlClient } from '~/sanity/client';
 
 export const loader: LoaderFunction = async () => {
+  // @todo: Fetch medium posts from rss feed and merge with sanity articles.
+  // @link https://medium.com/feed/@rshackleton
+
   const result = await graphqlClient.request<GetArticlesResult>(GetArticles);
 
   const articles = result.allArticle;
@@ -22,6 +26,14 @@ export const meta: MetaFunction = () => {
 export default function ArticlesIndex() {
   const articles = useLoaderData<ArticleModel[]>();
 
+  const formatter = new Intl.DateTimeFormat(undefined, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const builder = imageUrlBuilder(client);
+
   return (
     <div>
       <h1>Articles</h1>
@@ -29,7 +41,15 @@ export default function ArticlesIndex() {
         {articles.map((article) => (
           <li key={article._id}>
             <Link prefetch="intent" to={`/articles/${article.slug.current}`}>
-              {article.title}
+              <img
+                alt=""
+                src={builder.image(article.banner).width(400).url()}
+              />
+              <h2>{article.title}</h2>
+              <time dateTime={article.date}>
+                <small>{formatter.format(new Date(article.date))}</small>
+              </time>
+              <p>{article.summary}</p>
             </Link>
           </li>
         ))}
