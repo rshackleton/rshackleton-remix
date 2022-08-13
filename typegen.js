40edba4b7c1b930e25a063d7d52075639e46da19
Richard Shackleton <richard.shackleton@idhl.co.uk>
2022-08-13 12:29:27 +0100

    Update scripts

--- typegen.js
+++ typegen.js
@@ -1,6 +1,29 @@
+const { execSync } = require('child_process');
+const { renameSync } = require('fs');
 const storyblokToTypescript = require('storyblok-generate-ts');
 
+// Load .env file for dev
+require('dotenv').config();
+
+// Update component/present schema json
+execSync(
+  `storyblok pull-components --space ${process.env.STORYBLOK_SPACE_ID}`,
+  { stdio: 'inherit' },
+);
+
+// Move files to more convenient location
+renameSync(
+  `./components.${process.env.STORYBLOK_SPACE_ID}.json`,
+  `./app/storyblok/components.json`,
+);
+
+renameSync(
+  `./presets.${process.env.STORYBLOK_SPACE_ID}.json`,
+  `./app/storyblok/presets.json`,
+);
+
+// Generate types from components json
 storyblokToTypescript({
-  componentsJson: require('./components.170629.json'),
+  componentsJson: require(`./app/storyblok/components.json`),
   path: __dirname + '/app/storyblok/storyblok.d.ts',
 });
